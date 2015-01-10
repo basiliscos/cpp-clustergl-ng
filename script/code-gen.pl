c3c4e368fadb2333ad5bdd8b2ddba11fca841151
Ivan Baidakou <basiliscos@openmailbox.com>
2015-01-11 01:13:32 +0300

    Improve makefile and codegeneration interaction

--- script/code-gen.pl
+++ script/code-gen.pl
@@ -7,60 +7,71 @@ use ClusterGLNG::Parser qw/parse/;
 use ClusterGLNG::CodeGenerator qw/create_generator/;
 use Getopt::Long qw(GetOptions :config no_auto_abbrev no_ignore_case);
 use Path::Tiny;
+use Storable;
 
 GetOptions(
     'a|xml_ast=s'         => \my $xml_ast,
+    'c|cache_file=s'      => \my $cache_file,
     'f|function_filter=s' => \my $function_filter,
+    'r|role=s'            => \my $role,
     'o|output_dir=s'      => \my $output_dir,
     'h|help'              => \my $help,
 );
 
-my $show_help = $help || !$xml_ast || !$function_filter || !$output_dir ;
+my $show_help = $help || ($xml_ast && !($cache_file && $function_filter))
+    || (!$xml_ast && $cache_file && !($output_dir && $role));
 die <<"EOF" if($show_help);
 usage: $0 OPTIONS
   -a, --xml_ast          XML abstract syntax tree
+  -c, --cache_file       Cache file
   -f, --function_filter  Funtion filter, e.g. /^gl/
   -o, --output_dir       Output directory for the generated files
-  -h, --help          Show this message.
+  -r, --role             Role, i.e. declaration, packer, ...
+  -h, --help             Show this message.
 
   $0  Copyright 2014, Ivan Baidakou, Republic of Belarus
 EOF
 
-my $xml = path($xml_ast)->slurp;
-
-my ($functions, $typedefs) = parse($xml, qr/$function_filter/);
-print "parsing complete\n";
-
-{
-    my $generated_h = path($output_dir, 'generated.h');
-    print "generating $generated_h\n";
-    my $generated_h_fh = $generated_h->filehandle('>');
-    print $generated_h_fh <<START;
+if ($xml_ast && $cache_file) {
+    my $xml = path($xml_ast)->slurp;
+    my ($functions, $typedefs) = parse($xml, qr/$function_filter/);
+    print "parsing complete\n";
+    store [$functions, $typedefs], $cache_file;
+    print "Stored as cache $cache_file\n";
+}
+elsif ($role && $cache_file && $output_dir) {
+    my ($functions, $typedefs) = @{ retrieve($cache_file) };
+    print "$role\n";
+    if ($role eq 'declaration') {
+        my $file = path($output_dir, 'generated.h');
+        print "generating $file\n";
+        my $fh = $file->filehandle('>');
+        print $fh <<START;
 #ifndef _GENERATED_H
 #define _GENERATED_H
 
 #include "Instruction.h"
 
 START
-    create_generator($functions, $typedefs)
-        ->('declaration')->($generated_h_fh);
-    print $generated_h_fh <<END;
+        create_generator($functions, $typedefs)->('declaration')->($fh);
+        print $fh <<END;
 #endif /* GENERATED_H */
 END
-    print "$generated_h successfully created\n";
-}
-
-{
-    my $packer_c = path($output_dir, 'generated_packer.cpp');
-    print "generating $packer_c\n";
-    my $packer_c_fh = $packer_c->filehandle('>');
-    print $packer_c_fh <<START;
+        print "$file successfully created\n";
+    }
+    elsif ($role eq 'packer') {
+        my $file = path($output_dir, 'generated_packer.cpp');
+        print "generating $file\n";
+        my $fh = $file->filehandle('>');
+        print $fh <<START;
 #include "generated.h"
 #include "common.h"
+
 START
-    create_generator($functions, $typedefs)
-        ->('packer')->($packer_c_fh);
-    print "$packer_c successfully created\n";
+        create_generator($functions, $typedefs)->('packer')->($fh);
+        print "$file successfully created\n";
+    }
+    else {
+        die("Role $role isn't supported");
+    }
 }
-
-
